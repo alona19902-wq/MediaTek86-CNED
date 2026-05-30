be4a220a9dd5dda7fdc9820dbc11eeebb2d0ee98
Alona <alona19902@gmail.com>
2026-05-30 03:51:06 +0200

    Création de la classe metier Motif

--- MediaTek86/model/motif.cs
+++ MediaTek86/model/motif.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace MediaTek86.model
 {
-    internal class motif
+    /// <summary>
+    /// Classe métier des motifs
+    /// </summary>
+    public class Motif
     {
+        public int Idmotif { get; set; }
+        public string Libelle { get; set; }
+        
+        /// <summary>
+        /// Valorise les propriétés
+        /// </summary>
+        /// <param name= "idmotif"></param>
+        /// <param name= "libelle"></param>
+        
+        public Motif(int idmotif, string libelle)
+        {
+            this.Idmotif = idmotif;
+            this.Libelle = libelle;
+         
+        }
     }
+
 }
