01cfb4e741fbceed8a67fc8023e93185ea7cc7cc
Alona <alona19902@gmail.com>
2026-05-30 04:19:58 +0200

    Création de la classe métier Responsable et Service

--- MediaTek86/model/responsable.cs
+++ MediaTek86/model/responsable.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace MediaTek86.model
 {
-    internal class responsable
+    /// <summary>
+    /// Classe métier des responsable
+    /// </summary>
+    public class Responsable
     {
+        /// <summary>
+        /// Valorise les propriétés
+        /// </summary>
+        /// <param name= "login"></param>
+        /// <param name= "pwd"></param>
+       
+        public Responsable(string login, string pwd)
+        {
+            this.Login = login;
+            this.Pwd = pwd;
+          
+        }
+
+        public string Login { get;}
+        public string Pwd { get;}
+
     }
 }
+
--- MediaTek86/model/service.cs
+++ MediaTek86/model/service.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace MediaTek86.model
 {
-    internal class service
+    /// <summary>
+    /// Classe métier des service
+    /// </summary>
+    public class Service
     {
+        /// <summary>
+        /// Valorise les propriétés
+        /// </summary>
+        /// <param name= "idservice"></param>
+        /// <param name= "nom"></param>
+       
+        public Service (int idservice, string nom)
+        {
+            this.Idservice = idservice;
+            this.Nom = nom;
+        
+        }
+
+        public int Idservice { get; set; }
+        public string Nom { get; set; }
+        
     }
 }
